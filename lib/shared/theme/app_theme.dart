726a2cbb8cbdf02705b29027de24cc9e079bec2c
Lucas Almeida <lucasaclima03@gmail.com>
2021-12-08 13:43:03 -0300

    configuração dos temas do aplicativo

---
+++ lib/shared/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'app_colors.dart';
+import 'app_text_styles.dart';
+
+class AppTheme {
+  static final AppTheme instance = AppTheme();  
+
+  final _colors = AppColors();
+  static AppColors get colors => instance._colors;
+  final _textStyles = AppTextStyles();
+  static AppTextStyles get textStyles => instance._textStyles;
+}
